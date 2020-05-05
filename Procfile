ac0056a32fc6189ee68495e98e2b170e1ecf5c05
tismagulov <ismagulovt021@gmail.com>
2020-05-05 21:41:34 +0600

    stage2

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: python ./app/manage.py migrate
-web: python ./app/manage.py check --deploy
+web: gunicorn myproject.wsgi
--- app/library/settings.py
+++ app/library/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -148,3 +149,5 @@ LOGOUT_REDIRECT_URL = '/accounts/login'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+django_heroku.settings(locals())
